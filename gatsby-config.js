71280a31a2a82d73eebd8c20492373858432da6b
nickk <nickkochornswasdi@gmail.com>
2020-11-27 23:50:22 -0800

    removed path prefixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,5 +41,5 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-  pathPrefix: "/elijah-ford",
+  // pathPrefix: "/elijah-ford",
 }
